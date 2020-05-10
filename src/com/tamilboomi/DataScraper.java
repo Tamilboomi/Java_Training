113b58d2b1dd86dfbdaaa7c1657ce8b4edc2de28
ElamParithi Arul <elam@say-eyes.com>
2020-05-10 17:34:00 -0400

    changed code for getReportFromResponse

--- src/com/tamilboomi/DataScraper.java
+++ src/com/tamilboomi/DataScraper.java
@@ -3,12 +3,16 @@ package com.tamilboomi;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class DataScraper {
 
+    // Get data from API
     public static String getResponse(String fetchUrl) throws Exception {
         StringBuilder result = new StringBuilder();
         URL url = new URL(fetchUrl);
@@ -23,22 +27,12 @@ public class DataScraper {
         return result.toString();
     }
 
-    public static void writeToFile(String dataToWrite, String fileName) {
-        try {
-            FileWriter fileToWrite = new FileWriter(fileName);
-            fileToWrite.write(dataToWrite);
-            fileToWrite.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
-
+    // Parse the Data using JSON parsing library
     public static String getReportTextFromResponse(String response) {
         JSONObject jsonObject = new JSONObject(response);
         JSONArray dataArray = jsonObject.getJSONArray("data");
         StringBuilder sb = new StringBuilder();
-        sb.append("<table style='border: 1px solid black;'>");
+
         for (int i = 0; i < dataArray.length(); i++) {
             int id = dataArray.getJSONObject(i).getInt("id");
             String email = dataArray.getJSONObject(i).getString("email");
@@ -46,14 +40,20 @@ public class DataScraper {
             String lastName = dataArray.getJSONObject(i).getString("last_name");
             String avatar = dataArray.getJSONObject(i).getString("avatar");
 
-            sb.append("<tr>");
-            sb.append("<td>").append("ID: ").append(id).append("</td>");
-            sb.append("<td>").append("EMAIL: ").append(email).append("</td>");
-            sb.append("<td>").append("NAME: ").append(firstName).append(" ").append(lastName).append("</td>");
-            sb.append("<td>").append("PICTURE: ").append(avatar).append("</td>");
-            sb.append("</tr>");
+            sb.append("\n").append("ID: ").append(id).append("\n");
+            sb.append("EMAIL: ").append(email).append("\n");
+            sb.append("NAME: ").append(firstName).append(" ").append(lastName).append("\n");
+            sb.append("PICTURE: ").append(avatar).append("\n");
+            sb.append("--------------------------------------------");
         }
-        sb.append("</table>");
         return sb.toString();
     }
+
+    // Prepare the report and write to a file - sample.txt
+    public static void writeToFile(String dataToWrite, String fileName) throws IOException {
+        FileWriter fileToWrite = new FileWriter(fileName);
+        fileToWrite.write(dataToWrite);
+        fileToWrite.close();
+    }
+
 }
